2736422cc2052aa7ed9d61029fdcdafe7fd8791f
Waldemar Schwan <waldemarschwan@Waldemars-MacBook-Air.local>
2021-08-11 21:17:35 +0100

    style latest word addition differently

--- src/WordTreasure.tsx
+++ src/WordTreasure.tsx
@@ -2,19 +2,30 @@ import React, { FC } from 'react';
 import './WordTreasure.css';
 import { wordScore } from './game-rules';
 
-const WordTreasure: FC<{ words: string[] }> = ({ words }) => (
-  <div className="WordTreasure">
-    <ol>
-      {words
-        .sort((a, b) => wordScore(b) - wordScore(a))
-        .map((w) => (
-          <li key={w}>
-            <span className="WordTreasure_word">{w} </span>
-            <span className="WordTreasure_word_score">({wordScore(w)})</span>
-          </li>
-        ))}
-    </ol>
-  </div>
-);
+const WordTreasure: FC<{ words: string[] }> = ({ words }) => {
+  const indexed_words: Array<[string, number]> = words.map((item, index) => [
+    item,
+    index,
+  ]);
+  return (
+    <div className="WordTreasure">
+      <ol>
+        {indexed_words
+          .sort(([a, _aa], [b, _bb]) => wordScore(b) - wordScore(a))
+          .map(([w, index]) => (
+            <li
+              key={w}
+              className={
+                (index == words.length - 1 && 'WordTreasure_word--newest') || ''
+              }
+            >
+              <span className="WordTreasure_word">{w} </span>
+              <span className="WordTreasure_word_score">({wordScore(w)})</span>
+            </li>
+          ))}
+      </ol>
+    </div>
+  );
+};
 
 export default WordTreasure;
